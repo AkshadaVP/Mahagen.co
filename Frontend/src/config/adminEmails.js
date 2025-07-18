33a6c20dd37f20b9e06c00b51a4341166ad52207
alokbhoye <alokbhoye21@gmail.com>
2025-07-18 12:10:24 +0530

    final changes

--- Frontend/src/config/adminEmails.js
+++ Frontend/src/config/adminEmails.js
@@ -1,5 +1,5 @@
 // src/config/adminEmails.js
 export const adminUsers = [
   { email: "admin@maha-genco.co.in", role: "admin" },
-  { email: "shasank9272@gmail.com", role: "admin" }
+  { email: "shasank9272@gmail.com", role: "admin" },
 ];
